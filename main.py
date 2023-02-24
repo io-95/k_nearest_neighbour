d8f1b9c826b99324adedfb14ad08c052b4ab9732
io-171737 <ibrahim.oezel@hs-weingarten.de>
2023-02-24 21:07:03 +0100

    save amount of lines of test and data in gloabal variable

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import csv
 import math
 
+line_test = 0
+line_data = 0
+
 def main():
 	data = []
 	readfile(data)
 	normalization(data)
-	print(data)
+	
+
 
 
 def readfile(array):
+	global line_test
+	global line_data
+
 	with open('app1.data', newline='') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',')
 		for row in reader:
 			array.append([float(num) for num in row])
+
+	line_data = len(array)
 
 	with open('app1.test', newline='') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',')
 		for row in reader:
 			array.append([float(num) for num in row])
 
+	line_test = len(array) - line_data
+	
 def normalization(array):
 	for i in range(len(array[0])):
 		min = math.inf
